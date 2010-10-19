171b8fe7f67307e44849f219e7eacaa55060ed06
Felipe Elias Philipp <felipeelias@gmail.com>
2010-10-19 18:26:28 +0100

    tabs to spaces

--- examples/starfield.js
+++ examples/starfield.js
@@ -1,24 +1,24 @@
 function Starfield ( game, params ) {
-	var self = this, 
-	    params = params || {};
-	
-	self.xLimit = game.canvas.offsetWidth;
-	self.yLimit = game.canvas.offsetHeight;
-	self.starCount = params.starCount || 100;
-	
-	$.extend(self, {
-	  init: function() {
-    	self.stars = [];
-    	
-    	for(var i = 0; i < self.starCount; i++) {
+  var self = this, 
+      params = params || {};
+  
+  self.xLimit = game.canvas.offsetWidth;
+  self.yLimit = game.canvas.offsetHeight;
+  self.starCount = params.starCount || 100;
+  
+  $.extend(self, {
+    init: function() {
+      self.stars = [];
+      
+      for(var i = 0; i < self.starCount; i++) {
         self.stars.push(self.newStar());
         game.paper.fillStyle = rgb(self.stars[i].color, self.stars[i].color, self.stars[i].color);
-    	}
+      }
     },
     
     draw: function() {
-    	for(var i = 0; i < self.starCount; i++) {
-    		var star = self.stars[i];
+      for(var i = 0; i < self.starCount; i++) {
+        var star = self.stars[i];
 
         if ( star.y > self.yLimit ) {
          self.recycleStar(star);
@@ -26,61 +26,61 @@ function Starfield ( game, params ) {
          star.y += star.acc;
         }
 
-    		self.drawStar(star);
-    	}
+        self.drawStar(star);
+      }
     },
     
     drawStar: function ( star ) {
       game.paper.fillStyle = rgb(star.color, star.color, star.color);
       game.paper.beginPath();
-    	game.paper.arc(star.x, star.y, star.z, 0, Math.PI*2, true);
+      game.paper.arc(star.x, star.y, star.z, 0, Math.PI*2, true);
       game.paper.fill();
     },
     
     newStar: function() {
       return {
-  			x: self.newX(),
-  			y: self.newY(),
-  			z: self.newZ(),
-  			acc: self.newAcc(),
-  			color: self.newColor()
-  		}
+        x: self.newX(),
+        y: self.newY(),
+        z: self.newZ(),
+        acc: self.newAcc(),
+        color: self.newColor()
+      }
     },
     
     recycleStar: function ( star ) {
-    	star.x    = self.newX();
-    	star.y    = 0;
-    	star.z    = self.newZ();
-    	star.acc  = self.newAcc();
-    	star.color = self.newColor();
+      star.x    = self.newX();
+      star.y    = 0;
+      star.z    = self.newZ();
+      star.acc  = self.newAcc();
+      star.color = self.newColor();
     },
     
     newX: function() {
-    	return random(0, self.xLimit);
+      return random(0, self.xLimit);
     },
 
     newY: function() {
-    	return random(0, self.yLimit);
+      return random(0, self.yLimit);
     },
     
     newZ: function() {
-    	return 1;
+      return 1;
     },
     
     newAcc: function() {
-    	return random(1, 15);
+      return random(1, 15);
     },
     
     newColor: function() {
-    	return random(0, 255);
+      return random(0, 255);
     },
     
-	  toString: function() {
-    	return "Starfield#" + game.canvas.id;
+    toString: function() {
+      return "Starfield#" + game.canvas.id;
     }
-	});
-	
-	self.init();
+  });
+  
+  self.init();
 };
 
 function random(lower, upper){
