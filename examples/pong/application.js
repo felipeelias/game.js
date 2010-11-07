f9e1aa418a6d21915336b0e4cf184fdaac672371
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-07 19:15:07 +0000

    basic movement

--- examples/pong/application.js
+++ examples/pong/application.js
@@ -4,7 +4,13 @@ var player = new Paddle(game);
 $("#canvas").css({"background-color": "#000"});
 
 game.loop(function() {
+  if (isKeyPressed('up')) {
+    player.moveUp();
+  }  
   
+  if (isKeyPressed('down')) {
+    player.moveDown();
+  }  
   
   player.draw();
 });
--- examples/pong/paddle.js
+++ examples/pong/paddle.js
@@ -2,8 +2,10 @@ function Paddle( game ) {
   var self = this,
       player = {
         width: 10,
-        height: 100
-      };
+        height: 100,
+        velocity: 7
+      },
+      padding = 10;
   
   $.extend(self, {
     init: function() {
@@ -13,6 +15,25 @@ function Paddle( game ) {
     draw: function() {
       game.paper.fillStyle = "white";
       game.paper.fillRect(self.position.x, self.position.y, player.width, player.height);
+    },
+    
+    moveUp: function() {
+      self.position.y -= player.velocity;
+      self.checkBounds();
+    },
+    
+    moveDown: function() {
+      self.position.y += player.velocity;
+      self.checkBounds();
+    },
+    
+    checkBounds: function() {
+      if ( self.position.y >= (game.canvas.height - player.height - padding) ) {
+        self.position.y = game.canvas.height - player.height - padding;
+      }
+      if ( self.position.y <= padding ) {
+        self.position.y = padding;
+      }
     }
   });
   
