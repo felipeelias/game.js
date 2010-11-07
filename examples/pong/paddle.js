bea627ebc7283671ef3a3808a361a90f6cff58d7
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-07 19:19:41 +0000

    wrapped paddle class inside a closure

--- examples/pong/paddle.js
+++ examples/pong/paddle.js
@@ -1,41 +1,45 @@
-function Paddle( game ) {
-  var self = this,
-      player = {
-        width: 10,
-        height: 100,
-        velocity: 7
+(function() {
+  function Paddle( game ) {
+    var self = this,
+        player = {
+          width: 10,
+          height: 100,
+          velocity: 7
+        },
+        padding = 10;
+
+    $.extend(self, {
+      init: function() {
+        self.position = { x: 10, y: 10 };
       },
-      padding = 10;
-  
-  $.extend(self, {
-    init: function() {
-      self.position = { x: 10, y: 10 };
-    },
-    
-    draw: function() {
-      game.paper.fillStyle = "white";
-      game.paper.fillRect(self.position.x, self.position.y, player.width, player.height);
-    },
-    
-    moveUp: function() {
-      self.position.y -= player.velocity;
-      self.checkBounds();
-    },
-    
-    moveDown: function() {
-      self.position.y += player.velocity;
-      self.checkBounds();
-    },
-    
-    checkBounds: function() {
-      if ( self.position.y >= (game.canvas.height - player.height - padding) ) {
-        self.position.y = game.canvas.height - player.height - padding;
+
+      draw: function() {
+        game.paper.fillStyle = "white";
+        game.paper.fillRect(self.position.x, self.position.y, player.width, player.height);
+      },
+
+      moveUp: function() {
+        self.position.y -= player.velocity;
+        self.checkBounds();
+      },
+
+      moveDown: function() {
+        self.position.y += player.velocity;
+        self.checkBounds();
+      },
+
+      checkBounds: function() {
+        if ( self.position.y >= (game.canvas.height - player.height - padding) ) {
+          self.position.y = game.canvas.height - player.height - padding;
+        }
+        if ( self.position.y <= padding ) {
+          self.position.y = padding;
+        }
       }
-      if ( self.position.y <= padding ) {
-        self.position.y = padding;
-      }
-    }
-  });
+    });
+
+    self.init();
+  }
   
-  self.init();
-}
+  window.Paddle = Paddle;
+})();
