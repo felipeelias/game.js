5be85f619ebbdd4884d62037351a48b6402b294c
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-09 20:37:18 +0000

    canvas wrapper api

---
+++ js/canvas.js
@@ -0,0 +1,40 @@
+(function() {
+  function CanvasWrapper( canvasElement ) {
+    var context,
+        textDefaults;
+    
+    context = canvasElement.getContext('2d');
+    
+    textDefaults = {
+      align: 'start',
+      baseLine: 'alphabetic',
+      color: 'black',
+      font: '10px sans-serif'
+    };
+    
+    return {
+      context: context,
+      
+      draw: function( fn ) {
+        context.save();
+        fn(context);
+        context.restore();
+      },
+      
+      text: function(text, x, y, options) {
+        var options = $.extend({}, textDefaults, options);
+        
+        this.draw(function(c) {
+          c.fillStyle = options.color;
+          c.font = options.font;
+          c.textAlign = options.align;
+          c.fillText(text, x, y);
+        });
+        
+        return this;
+      }
+    };
+  }
+  
+  window.CanvasWrapper = CanvasWrapper;
+})()
