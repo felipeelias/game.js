4ca6b6437bb2a4a22926fe5da52e2b1efe498975
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-30 23:44:10 +0000

    testing width, heigth and center orientations points

--- test/canvas_test.js
+++ test/canvas_test.js
@@ -1,6 +1,6 @@
 module("Canvas wrapper", {
   setup: function() {
-    $("#qunit-fixture").append("<canvas id=\"canvas\"></canvas>");
+    $("#qunit-fixture").append("<canvas id=\"canvas\" width=\"400\", height=\"300\"></canvas>");
     this.canvas = new CanvasWrapper(document.getElementById("canvas"));
   }
 });
@@ -11,4 +11,11 @@ test("the paper property should be an canvas wrapper", 1, function() {
 
 test("the default context should be 2d", 1, function() {
   same(this.canvas.context, document.getElementById("canvas").getContext("2d"));
+});
+
+test("check for the height, width and center orientation", 4, function() {
+  equal(this.canvas.width, 400);
+  equal(this.canvas.height, 300);
+  equal(this.canvas.centerX, 200);
+  equal(this.canvas.centerY, 150);
 });
