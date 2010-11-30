f4d6a658771731c66392993aa62a53cbfa2dcc8c
Felipe Elias Philipp <felipeelias@gmail.com>
2010-11-30 23:27:58 +0000

    testing canvas wrapper class and not the game class

--- test/canvas_test.js
+++ test/canvas_test.js
@@ -1,9 +1,13 @@
 module("Canvas wrapper", {
   setup: function() {
-    this.game = new Game();
+    this.canvas = new CanvasWrapper(document.getElementById("canvas"));
   }
 });
 
 test("the paper property should be an canvas wrapper", 1, function() {
-  ok($.isFunction(this.game.paper.draw));
+  ok($.isFunction(this.canvas.draw));
+});
+
+test("the default context should be 2d", 1, function() {
+  same(this.canvas.context, document.getElementById("canvas").getContext("2d"));
 });
