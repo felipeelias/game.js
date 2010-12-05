4762be4b8dda52537d805200d6f8633e07b0af10
Felipe Elias Philipp <felipeelias@gmail.com>
2010-12-05 17:51:45 +0000

    default sound class functionality

--- js/sound.js
+++ js/sound.js
@@ -6,8 +6,35 @@
         'ogg': 'audio/ogg'
       }
   
-  function Sound() {
-    
+  function Sound( file_name ) {
+    var path,
+        audio_element;
+        
+    path = file_name + Sound.default_file_extension;
+    audio = new Audio(path);
+    //starts with preload none and src setted
+    //#load sets the preload to metadata
+    //#play plays
+    //#stop pauses
+    //events listeners: onloadstart, onloadend, onplay, onstop
+    return {
+      name: file_name,
+      path: path,
+      audio_element: audio,
+      play: function() {
+        
+      },
+      
+      loadEnd: function( fn ) {
+        audio.addEventListener('loadedmetadata', function() {
+          fn.call(this);
+        });
+      },
+      
+      preload: function() {
+        audio.preload = "metadata";
+      }
+    };
   }
 
   Sound.hasSupport = !!audio_element.canPlayType;
@@ -15,6 +42,8 @@
   Sound.hasSupportFor = function( file_type ) {
     return !!this.hasSupportFor && "" !== audio_element.canPlayType(file_mime_types[file_type]);
   }
+  
+  Sound.default_file_extension = Sound.hasSupportFor('mp3') ? '.mp3' : '.ogg';
   
   window.Sound = Sound;
 })();
--- test/sound_test.js
+++ test/sound_test.js
@@ -12,6 +12,16 @@ if ($.browser.safari) {
     ok(Sound.hasSupportFor('wave'), "supports wave files");
     ok(!Sound.hasSupportFor('ogg'), "doesn't support ogg files");
   });
+  
+  test("default file extension", 1, function() {
+    equals(Sound.default_file_extension, ".mp3", "mp3 file extension");
+  });
+
+  test("file path", 1, function() {
+    var sound = new Sound('fixtures/sound1');
+    
+    equals(sound.path, "fixtures/sound1.mp3", "sound path");
+  });
 }
 
 if ($.browser.mozilla) {
@@ -22,4 +32,44 @@ if ($.browser.mozilla) {
     ok(Sound.hasSupportFor('wave'), "supports wave files");
     ok(Sound.hasSupportFor('ogg'), "supports ogg files");
   });
+
+  test("default file extension", 1, function() {
+    equals(Sound.default_file_extension, ".ogg", "ogg file extension");
+  });
 }
+
+module("Creating sounds");
+
+test("should respond to sound functions", 1, function() {
+  var sound = new Sound('fixtures/sound1');
+  
+  ok( $.isFunction(sound.play), "play function" );
+});
+
+test("should have the defaults attributes", 2, function() {
+  var sound = new Sound('fixtures/sound1');
+  
+  equals( sound.name, "fixtures/sound1", "sound name" );
+  equals( sound.audio_element.tagName, "AUDIO", "audio tagname" );
+});
+
+asyncTest("should not load the file when created", 0, function() {
+  var sound = new Sound('fixtures/sound1');
+  
+  sound.loadEnd(function() {
+    ok(false);
+  });
+  
+  start();
+});
+
+asyncTest("should load the file after preload function", 0, function() {
+  var sound = new Sound('fixtures/sound1');
+  
+  sound.loadEnd(function() {
+    ok(true);
+  });
+  
+  sound.preload();
+  start();
+});
